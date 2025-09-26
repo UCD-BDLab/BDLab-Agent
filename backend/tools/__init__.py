21f738fccfc27decd5a146110b619bee56ea4960
Vicente <ramosvicente255@gmail.com>
2025-09-26 10:49:45 -0600

    Small sample classification test

--- backend/tools/__init__.py
+++ backend/tools/__init__.py
@@ -1,5 +1,5 @@
-from .retrieval import chat_oss, ask_rag, retrieve_context
+from .retrieval import ask_rag, retrieve_context
 from .embed import text_splitter, read_files
-from .agent import load_llm, chat_oss
 
-__all__ = ["chat_oss", "ask_rag", "retrieve_context", "load_llm", "text_splitter", "read_files"]
+
+__all__ = ["ask_rag", "retrieve_context", "load_llm", "text_splitter", "read_files"]
